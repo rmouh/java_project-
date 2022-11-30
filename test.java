a54dc558611b865ca040647135a2d8bb6192eb3b
ClementFenneteaux <118749822+ClementFenneteaux@users.noreply.github.com>
2022-11-30 13:19:38 +0100

    quelques modif et tp9 fini ?

---
+++ test.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class test {
+    public static void main ( String [] args ) {
+        JFrame frame = new JFrame ( "test" );
+        JPanel panel = new JPanel();
+        frame.getContentPane().add( panel );
+        frame.pack ();
+        frame.setVisible ( true );
+        frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+    
+    }
+        
+}
